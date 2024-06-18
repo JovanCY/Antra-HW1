ae8fbdb4a6a0e51326ed5a658b60ffd910112066
JovanCY <97321359+JovanCY@users.noreply.github.com>
2024-06-18 01:02:24 -0700

    HW4 update

--- HW4/arrays.js
+++ HW4/arrays.js
@@ -16,7 +16,7 @@ if (array.includes(100, 0)) {
 console.log("contents of the array: ", array.join(", ")); //join for printing
 console.log("popping from array: ", array.pop());
 console.log("contents of the array: ", array.join(", "));
-console.log("reversed: ", array.reverse());//reverse acts on the original array
+console.log("reversed: ", array.reverse()); //reverse acts on the original array
 array.reverse();
 
 const evenArray = array.filter((number) => {
@@ -64,8 +64,7 @@ for (let i = 0; i < 10; i++) {
 console.log("random array = ", randomArray);
 randomArray.sort();
 console.log("random array = ", randomArray);
-console.log("random array sliced = ", randomArray.slice(0,5));
-
+console.log("random array sliced = ", randomArray.slice(0, 5));
 
 // other useful ones
 // some, every, findIndex,
@@ -104,28 +103,57 @@ console.log(
 // static:
 // isInteger, isNaN, parseInt, parseFloat
 
-console.log("parsing int abc123",parseInt("abc123"))
-console.log("parsing int 123abc",parseInt("123abc"))
-console.log("parsing int 123abc123",parseInt("123abc123"))
+console.log("parsing int abc123", parseInt("abc123"));
+console.log("parsing int 123abc", parseInt("123abc"));
+console.log("parsing int 123abc123", parseInt("123abc123"));
 
-console.log("checking if 123 isInteger",Number.isInteger(123))
-console.log("checking if 123 isInteger",Number.isInteger("123"))
-console.log("checking if 123 isInteger",Number.isInteger("123abc"))
-console.log("checking if 123 isInteger",Number.isInteger(parseInt("123abc")))
+console.log("checking if 123 isInteger", Number.isInteger(123));
+console.log("checking if 123 isInteger", Number.isInteger("123"));
+console.log("checking if 123 isInteger", Number.isInteger("123abc"));
+console.log("checking if 123 isInteger", Number.isInteger(parseInt("123abc")));
 
+console.log("checking if '123' isNaN ", isNaN("123"));
+console.log("checking if '123abc' isNaN ", isNaN("123abc"));
+console.log("checking if 'abc123' isNaN ", isNaN("abc123"));
 // must know Strings
 // split, toUpperCase, toLowerCase
 // useful ones:
 // includes, charAt, slice, replace, substring, trim
 
-let originalString = "This is the Original String"
+let originalString = "   This is the Original String   ";
 
-console.log(originalString)
-console.log(originalString.split(" "))
-console.log(originalString.toUpperCase())
-console.log(originalString.toLowerCase())
-console.log(originalString)   //original string is not changed by toUppercase and toLowerCase
+console.log(originalString);
+console.log(originalString.split(" "));
+console.log(originalString.toUpperCase());
+console.log(originalString.toLowerCase());
+console.log(originalString); //original string is not changed by toUppercase and toLowerCase
 
+console.log(originalString.replace(" ", "*")); //replaces first instance only
+console.log(originalString.substring(5));
+console.log(originalString.trim());
+console.log(originalString.charAt(6));
 // must know Object
 // static
 // entries, keys, values
+
+const obj1 = {
+  name: "Jovan",
+};
+
+//const
+class testObject {
+  prop1;
+  prop2;
+  constructor(input1, input2) {
+    this.prop1 = input1;
+    this.prop2 = input2;
+  }
+
+  getProp1() {
+    return this.prop1;
+  }
+}
+
+const testing = new testObject("hahaha", "this is a test");
+
+console.log("prop1 of testing obj", testing.getProp1());
